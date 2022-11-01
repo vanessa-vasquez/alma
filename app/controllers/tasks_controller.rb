1ff8be3a8ecc8f17578ff161cb5a9bf4d98a34fa
Vanessa Vasquez <vvasquez528@gmail.com>
2022-11-01 13:19:35 -0400

    Fix redirect bug

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,9 @@
 class TasksController < ApplicationController
   def show
     if !user_signed_in?
-      redirect_to root_path
+      return redirect_to root_path
+    elsif Task.where(id: params[:id]).empty? || (!Task.where(id: params[:id]).empty? && (Task.find(params[:id]).user_id != current_user.id))
+      return redirect_to root_path
     end
 
     @task = Task.find params[:id]
@@ -14,15 +16,12 @@ class TasksController < ApplicationController
   end
 
   def index
-
     if (!user_signed_in?)
-      redirect_to root_path
+      return redirect_to root_path
     end
 
     if Task.joins(:user).exists?(:users => {school: current_user.school})
-      if !@user.school.blank?
-        @tasks = Task.joins(:user).where(:users => {school: current_user.school})
-      end
+      @tasks = Task.joins(:user).where(:users => {school: current_user.school})
     else
       @tasks = []
     end
@@ -30,7 +29,7 @@ class TasksController < ApplicationController
 
   def new
     if !user_signed_in?
-      redirect_to root_path
+      return redirect_to root_path
     end
     @user_id = current_user.id
   end
@@ -43,7 +42,9 @@ class TasksController < ApplicationController
 
   def edit
     if !user_signed_in?
-      redirect_to root_path
+      return redirect_to root_path
+    elsif Task.where(id: params[:id]).empty? || (!Task.where(id: params[:id]).empty? && (Task.find(params[:id]).user_id != current_user.id))
+      return redirect_to root_path
     end
 
     @task = Task.find params[:id]
@@ -75,7 +76,7 @@ class TasksController < ApplicationController
 
   def my_profile
     if !user_signed_in?
-      redirect_to root_path
+      return redirect_to root_path
     end
     @my_tasks = Task.where(user_id: current_user.id) == nil ? [] : Task.where(user_id: current_user.id)
     @first_name = current_user.fname
