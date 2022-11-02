1d02ba0cda028d7830847be3c3c86c323c2b0280
Aditi Dam <dama2468@gmail.com>
2022-11-01 20:59:30 -0400

    Continue to improve coverage

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
-describe Users::RegistrationsController do 
+describe DashboardController do 
     describe "Create #create" do
     let!(:user) {User.create!({id: 40, email: 'zg25@columbia.edu', password: 'password123', fname: 'Zeek', lname: 'Roll', school: 'Columbia University'})}
         before :each do
             @request.env['devise.mapping'] = Devise.mappings[:user]
-            sign_in user
+        end
+        it "checks if user is signed in" do
+            user1 = User.create(email: 'ad25@columbia.edu', password: 'CatsAreKool1', fname: 'Adam', lname: 'Daniels', school: 'Columbia University')
+            sign_in user1
         end
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe 'user model' do
+    let!(:user) {User.create!({id: 40, email: 'zg25@columbia.edu', password: 'password123', fname: 'Zeek', lname: 'Roll', school: 'Columbia University'})}
+    context "it has a user" do
+      it "returns the correct attributes" do
+        expect(user.id).to eql(40)
+        expect(user.email).to eql('zg25@columbia.edu')
+      end
+    end
+
+  end
+
+
+
+end 
