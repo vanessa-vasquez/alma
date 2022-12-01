cae54c45f921075dade87a3b5f48daf5fab137c4
ayshajamjam <aj2604@columbia.edu>
2022-11-30 23:05:58 -0500

    Removed acceptor id from cards and added acceptor email when viewing the taskinstead of acceptor id

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,9 @@ class TasksController < ApplicationController
     @price = @task.price
     @created_at = @task.created_at
     @user_accepted_id = @task.user_accepted_id
-
+    
+    @user = User.find @user_accepted_id.to_i
+    @user_accepted_uni = @user.email
   end
  
   def index
