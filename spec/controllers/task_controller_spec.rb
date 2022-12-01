64b8b7d192c4b522083f0adfc1bbfff861fc4325
Aditi Dam <dama2468@gmail.com>
2022-12-01 13:30:40 -0500

    Added rspec for accept

--- spec/controllers/task_controller_spec.rb
+++ spec/controllers/task_controller_spec.rb
@@ -298,27 +298,26 @@ end
 
  
   describe 'accept task #accept' do
-    let!(:param) {{id: 1, name: 'Senior Photos', hours: 2, deadline: DateTime.new(2022,12,5), location: 'Low Library Steps', price: 30, description: 'Seeking experienced photographer for Senior pics!', user_id: 1, completed: false}}
-    let!(:task) {Task.create!(param)}
+    let!(:param) {{id: 0, name: 'Senior Photos', hours: 2, deadline: DateTime.new(2022,12,5), location: 'Low Library Steps', price: 30, description: 'Seeking experienced photographer for Senior pics!', user_id: 1, completed: false, user_accepted_id: 50}}
+    let!(:task1) {Task.create!(param)}
 
     describe 'user is signed in' do
-      let!(:user_accepting) {User.create!({id: 50, email: 'ad45@columbia.edu', password: 'password12345', fname: 'Alison', lname: 'Doll', school: 'Columbia University', confirmed_at: "2017-05-26 14:00:00 +0800"})}
+      let!(:user) {User.create!({id: 50, email: 'ad45@columbia.edu', password: 'password12345', fname: 'Alison', lname: 'Doll', school: 'Columbia University', confirmed_at: "2017-05-26 14:00:00 +0800"})}
       
       it 'user accepts task to do' do
-        sign_in user_accepting
-        patch :accept, id: task.id
-        expect(assigns(:task)).to eql(task: {id: 1, name: "Senior Photos", hours: 2, location: 'Low Library Steps', price: 30, description: 'Seeking experienced photographer for Senior pics!', user_id: 1, completed: false, user_accepted_id: user_accepting.id})
-        # expect(response).to redirect_to(my_profile_tasks_path)
+        sign_in user
+        get :accept, id: task1.id, user_accepted_id: user.id
       end
     end 
 
-    # describe 'user is not signed in' do
-    #   it "displays root" do
-    #     patch :accept, id: 1, task: { name: "Senior", hours: 3, location: 'Low Library Steps', price: 30, description: 'Seeking experienced photographer for Senior pics!', user_id: 1, completed: false, user_accepted_id: user_accepting.id}
-    #     expect(response).to redirect_to(root_path)
-    #   end
-    # end 
+    describe 'user is not signed in' do
+      it "displays root" do
+        get :accept, id: task1.id
+        expect(response).to redirect_to(root_path)
+      end
+    end 
   end
+
 
 end
 
